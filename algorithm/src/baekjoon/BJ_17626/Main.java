18f155dc68dceff4a293c96c6ebaf92078eb7f3c
oh in seong <superohinsung@naver.com>
2023-02-13 11:29:02 +0900

    백준알고리즘 17626번

---
+++ algorithm/src/baekjoon/BJ_17626/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.BJ_17626;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N = 0;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 1];
+        dp[1] = 1;
+
+        int min = 0;
+        for (int i = 2; i <= N; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                int temp = i - j * j;
+                min = Math.min(min, dp[temp]);
+            }
+
+            dp[i] = min + 1;
+        }
+
+
+        System.out.println(dp[N]);
+    }
+}
