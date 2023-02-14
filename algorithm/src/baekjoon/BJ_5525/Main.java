2db5d443ff0daf4f130c8ee6808bf87125b9a75c
oh in seong <superohinsung@naver.com>
2023-02-14 10:42:37 +0900

    백준알고리즘 5525번

---
+++ algorithm/src/baekjoon/BJ_5525/Main.java
@@ -0,0 +1,35 @@
+package baekjoon.BJ_5525;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        char s[] = br.readLine().toCharArray();
+
+        int result = 0;
+        int count = 0;
+
+        for(int i=1; i < M - 1; i++) {
+            if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+
+                if(count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        System.out.println(result);
+
+    } // End of main
+} // End of class
