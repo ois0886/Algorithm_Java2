8845d0612540cc570f116e864d21f317106389e1
oh in seong <superohinsung@naver.com>
2023-02-20 15:57:59 +0900

    백준알고리즘 10026번 적록색약

--- algorithm/src/baekjoon/BJ_10026/Main.java
+++ algorithm/src/baekjoon/BJ_10026/Main.java
@@ -8,25 +8,25 @@ public class Main {
 
     static int N;
     static String S;
-    static char map[][];
-    static boolean visits[][];
-    static int dx[] = {-1, 0, 0, 1};
-    static int dy[] = {0, 1, -1, 0};
+    static char[][] arr;
+    static boolean[][] visits;
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, 1, -1, 0};
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new char[N + 1][N + 1];
+        arr = new char[N + 1][N + 1];
         visits = new boolean[N + 1][N + 1];
 
         for (int i = 0; i < N; i++) {
             S = br.readLine(); // RRRBB
             for (int j = 0; j < N; j++) {
-                map[i][j] = S.charAt(j); // R R R B B
+                arr[i][j] = S.charAt(j); // R R R B B
             }
         }
 
-        // normal 인 경우
+        // 정상인 경우
         int cnt = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -40,13 +40,13 @@ public class Main {
         cnt = 0;
         visits = new boolean[N + 1][N + 1];
 
-        // dltonism 인 경우
+        // 비정상인 경우
         // G를 R로 바꿔주고 돌린다.
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 'G') {
-                    map[i][j] = 'R'; // G를 R로 바꿔준다.
+                if (arr[i][j] == 'G') {
+                    arr[i][j] = 'R'; // G를 R로 바꿔준다.
                 }
             }
         }
@@ -67,7 +67,7 @@ public class Main {
 
     public static void dfs(int x, int y) {
         visits[x][y] = true;
-        char tmp_char = map[x][y]; // R
+        char tmp_char = arr[x][y]; // R
         for (int i = 0; i < 4; i++) {
             int new_x = x + dx[i];
             int new_y = y + dy[i];
@@ -76,7 +76,7 @@ public class Main {
                 continue;
             }
 
-            if (!visits[new_x][new_y] && map[new_x][new_y] == tmp_char) {
+            if (!visits[new_x][new_y] && arr[new_x][new_y] == tmp_char) {
                 dfs(new_x, new_y);
             }
         }
