e2a9824baac58caf4e59ad2a237d63311111c517
oh in seong <superohinsung@naver.com>
2023-02-28 20:28:09 +0900

    백준알고리즘 24264번 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ algorithm/src/baekjoon/BJ_24264/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.BJ_24264;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준알고리즘 24264번 알고리즘 수업 - 알고리즘의 수행 시간 3
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n);
+        System.out.println(2);
+
+    }
+}
