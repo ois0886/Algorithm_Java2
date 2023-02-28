9a770e8c130440e7e9e92a28a05d76f2a5f0570b
oh in seong <superohinsung@naver.com>
2023-02-28 15:26:31 +0900

    백준알고리즘 24262번 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ algorithm/src/baekjoon/BJ_24262/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.BJ_24262;
+
+// 백준알고리즘 24262번 알고리즘 수업 - 알고리즘의 수행 시간 1
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println('1');
+        System.out.println('0');
+    }
+}
